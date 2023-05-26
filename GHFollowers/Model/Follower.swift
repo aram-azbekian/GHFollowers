b4cf64097b0520261ea9d0b8ad0c38b7f76b925b
aram.azbekyan <aram.azbekyan@gs-labs.ru>
2023-05-26 12:19:30 +0300

    Bug fixes and model creation

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by aram.azbekyan on 26.05.2023.
+//
+
+import Foundation
+
+struct Follower: Codable {
+	var login: String
+	var avatarUrl: String
+}
